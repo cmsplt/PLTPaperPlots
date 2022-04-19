0c3d43ff89e0361626c175bf13a2fad0df2f421d
Paul Lujan <paul.lujan@cern.ch>
2022-04-19 14:45:15 +0200

    remove "Preliminary" from Fig. 11

--- MaskStudies/PlotAccidentalRatesMasks.C
+++ MaskStudies/PlotAccidentalRatesMasks.C
@@ -167,7 +167,7 @@ void PlotAccidentalRatesMasks(void) {
   t2->SetY(0.90);
   t2->SetTextFont(52);
   t2->SetTextSize(0.05);
-  t2->Draw();
+  // t2->Draw();
   TText *t3 = new TText(0, 0, "2016");
   t3->SetNDC();
   t3->SetX(0.10);
